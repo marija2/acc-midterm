810e0ee50bcfc16398fade5a24eadc86f2ccaa37
Marija Trifkovic <mt3786@nyu.edu>
2021-03-10 14:32:40 -0500

    added config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key:         'hjqEFOKgF3QDwiY11cjvGXwIe',
+    consumer_secret:      'kwOZCazPURrifFYuj0Gp6Z2zbAlvvVUGKk2gBzZwUDgguQ0GlF',
+    access_token:         '1366485236473806850-uvquVmXxlvsGBXftoJcVAXBJHC4gyq',
+    access_token_secret:  'YezQCD6HSg0vq4gXLBI6DkMRPagUqTIjnltLkmUXLTMFe',
+};
