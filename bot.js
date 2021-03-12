15ccfed6790570b1f3a09fc94040248c9c77c271
Marija Trifkovic <mt3786@nyu.edu>
2021-03-12 07:02:34 -0500

    fixed env

--- bot.js
+++ bot.js
@@ -12,10 +12,10 @@ var location;
 //var T = new Twit( config );
 
 var T = new Twit ( {
-  consumer_key:         process.env('CONSUMER_KEY'),
-  consumer_secret:      process.env('CONSUMER_SECRET'),
-  access_token:         process.env('ACCESS_TOKEN'),
-  access_token_secret:  process.env('ACCESS_TOKEN_SECRET'),
+  consumer_key:         process.env['CONSUMER_KEY'],
+  consumer_secret:      process.env['CONSUMER_SECRET'],
+  access_token:         process.env['ACCESS_TOKEN'],
+  access_token_secret:  process.env['ACCESS_TOKEN_SECRET'],
 } );
 
 var date = new Date();
