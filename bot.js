4fa3332fbea06e2480b6cdb668ce4d63189391c5
Marija Trifkovic <mt3786@nyu.edu>
2021-03-14 09:07:55 -0400

    finds breed

--- bot.js
+++ bot.js
@@ -2,11 +2,11 @@
 // pulling in the twit package
 const Twit = require ( 'twit' );
 
-//const config = require('./config.js');
+// const config = require('./config.js');
 const request = require ( 'request' );
 const fs = require ( 'fs' );
 
-//var T = new Twit( config );
+// var T = new Twit( config );
 var T = new Twit ( {
 
   consumer_key:         process.env['CONSUMER_KEY'],
@@ -26,14 +26,23 @@ var tweetText;
 
 // to get a random dog fact
 var getDogFactUrl = 'https://dog-facts-api.herokuapp.com/api/v1/resources/dogs?number=1';
+// to get a list of all dog breeds
+var getAllDogBreeds = 'https://dog.ceo/api/breeds/list/all';
+// to get an image of a specific breed
+var getImgOfBreedBase = 'https://dog.ceo/api/breed/';
 // to get a random dog image
-var getDogImageUrl = 'https://dog.ceo/api/breeds/image/random';
+var getRandomDogImageUrl = 'https://dog.ceo/api/breeds/image/random';
 
 var dogFactData;
 var dogImageData;
 var encodedDogImg;
 var mediaIdStr;
 var altText;
+var imgUrl;
+var found = false;
+var lowerCaseTweet;
+var allDogBreedsData;
+var arrDogBreeds;
 
 //call the first time
 tweetFromBot();
@@ -50,7 +59,7 @@ function tweetFromBot ( error, data, response ) {
   // got the random dog fact
   function gotDogFact ( error, response, body ) {
 
-    dogFactData = JSON.parse ( body )
+    dogFactData = JSON.parse ( body );
 
     // heroku 5 hours ahead
     currentDate = ( date.getMonth() + 1 ) + "/" + date.getDate() + "/" + date.getFullYear();
@@ -61,63 +70,97 @@ function tweetFromBot ( error, data, response ) {
     // if fact is longer then 140 chars, will not tweet
     if ( tweetText.length > 140 ) return;
 
-    // ask for a random dog image
-    request ( getDogImageUrl, gotDogImage );
+    lowerCaseTweet = tweetText.toLowerCase();
 
-    // got the random dog image
-    function gotDogImage ( error, response, body  ) {
-      
-      dogImageData = JSON.parse ( body );
+    // ask for a list of all dog breeds
+    request ( getAllDogBreeds , gotAllDogBreeds );
 
-      // if no image, don't tweet
-      if ( dogImageData.status != 'success' ) return;
+    // got all dog breeds
+    function gotAllDogBreeds ( error, response, body ) {
 
-      // for the name of the image
-      currentDate = ( date.getMonth() + 1 ) + date.getDate() + date.getFullYear();
-      currentTime = date.getHours() + date.getMinutes() + date.getSeconds();
+      allDogBreedsData = JSON.parse ( body );
 
-      // download dog image to laptop
-      downloadDogImage ( dogImageData.message, 'images/img' + currentDate + currentTime + '.png' );
+      if ( allDogBreedsData.status != 'success' ) return;
 
-      function downloadDogImage ( imgUrl, filename ) {
+      // because dog breeds are keys in allDogBreedsData
+      arrDogBreeds = Object.keys ( allDogBreedsData.message );
 
-        request.head ( imgUrl, downloadedImg );
+      // check if a dog breed is mentioned in the fact
+      for ( var i = 0; i < arrDogBreeds.length; ++i ) {
 
-        function downloadedImg ( error, response, body ) {
-          // need to close stream
-          request ( imgUrl ).pipe ( fs.createWriteStream ( filename ) ).on ( 'close', finished );
+        // if yes, will get an image of that breed
+        if ( lowerCaseTweet.includes( arrDogBreeds[i] ) ) {
+
+          imgUrl = getImgOfBreedBase + arrDogBreeds[i] + '/images/random';
+
+          found = true;
+
+          break;
         }
+      }
 
-        function finished () {
+      // if no, will get a random image
+      if ( !found ) imgUrl = getRandomDogImageUrl;
 
-          // encoding thhe image
-          encodedDogImg = fs.readFileSync ( filename, { encoding: 'base64' } );
+      // ask for a dog image
+      request ( imgUrl, gotDogImage );
 
-          // post just the image to twitter
-          T.post('media/upload', { media_data: encodedDogImg }, uploadedImg );
+      // got the dog image
+      function gotDogImage ( error, response, body  ) {
+        
+        dogImageData = JSON.parse ( body );
 
-          // image posted to twitter
-          function uploadedImg ( error, data, response ) {
+        // if no image, don't tweet
+        if ( dogImageData.status != 'success' ) return;
 
-            // get the id
-            mediaIdStr = data.media_id_string;
-            // set alt text
-            altText = "This image depicts a dog";
-              
-            T.post ( 'media/metadata/create', { media_id: mediaIdStr, alt_text: altText }, createdMedia );
+        // for the name of the image
+        currentDate = ( date.getMonth() + 1 ) + date.getDate() + date.getFullYear();
+        currentTime = date.getHours() + date.getMinutes() + date.getSeconds();
 
-            function createdMedia ( error, data, response ) {
+        // download dog image to laptop
+        downloadDogImage ( dogImageData.message, 'images/img' + currentDate + currentTime + '.png' );
 
-              // post the text and the uploaded image
-              T.post('statuses/update', { status: tweetText, media_ids: mediaIdStr }, tweeted );
+        function downloadDogImage ( imgUrl, filename ) {
 
-              // posted the text and the image
-              function tweeted ( error, data, response ) {
+          request.head ( imgUrl, downloadedImg );
 
-                // if something went wronr, print error
-                if ( error ) console.log ( error );
-                // else print what was posted
-                else console.log ( data.text );
+          function downloadedImg ( error, response, body ) {
+            // need to close stream
+            request ( imgUrl ).pipe ( fs.createWriteStream ( filename ) ).on ( 'close', finished );
+          }
+
+          function finished () {
+
+            // encoding thhe image
+            encodedDogImg = fs.readFileSync ( filename, { encoding: 'base64' } );
+
+            // post just the image to twitter
+            T.post('media/upload', { media_data: encodedDogImg }, uploadedImg );
+
+            // image posted to twitter
+            function uploadedImg ( error, data, response ) {
+
+              // get the id
+              mediaIdStr = data.media_id_string;
+              // set alt text
+              altText = "This image depicts a dog";
+                
+              T.post ( 'media/metadata/create', { media_id: mediaIdStr, alt_text: altText }, createdMedia );
+
+              function createdMedia ( error, data, response ) {
+
+                // post the text and the uploaded image
+                T.post('statuses/update', { status: tweetText, media_ids: mediaIdStr }, tweeted );
+
+                // posted the text and the image
+                function tweeted ( error, data, response ) {
+
+                  // if something went wronr, print error
+                  if ( error ) console.log ( error );
+                  // else print what was posted
+                  else console.log ( data.text );
+
+                }
 
               }
 
@@ -129,7 +172,7 @@ function tweetFromBot ( error, data, response ) {
 
       }
 
-    }
+    } 
 
   }
 
